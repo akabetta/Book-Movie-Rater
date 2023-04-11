3b7ccbd540f721de390e8e30cb48780115549dbb
akabetta <omerr_ylmz@hotmail.com>
2023-04-11 18:35:43 +0300

    index page done

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/books')
+def books():
+    return render_template('books.html')
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
 
 if __name__=='__main__':
     app.run(debug=True)
